f758adffced6bf7462deae0c1a7da9490160605c
Christian <christian151.bravo@gmail.com>
2020-10-18 21:58:54 -0600

    :rocket: (update site mode)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default {
   ** Nuxt rendering mode
   ** See https://nuxtjs.org/api/configuration-mode
   */
+  mode: 'spa',
   /*
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
