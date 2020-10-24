0042283dd074a329521a8e03db2e36d42ef2155d
Christian <christian151.bravo@gmail.com>
2020-10-23 20:58:43 -0600

    :bug: (adding fallback page)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,9 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'static',
+  generate: {
+    fallback: true,
+  },
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
