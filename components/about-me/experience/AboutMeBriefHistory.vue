6bfc4e7d7798f636f3eabea7cb63a73c6e36a8da
Christian Bravo <christian151.bravo@gmail.com>
2021-01-25 20:48:01 -0600

    :sparkles: (Adding new components for work experience)

---
+++ components/about-me/experience/AboutMeBriefHistory.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="min-h-screen | flex | p-20">
+    <div class="flex flex-col | w-full">
+      <h2 class="section-title about-me-brief-history__title">
+        {{ $t('me.work-experience') }}
+      </h2>
+      <div class="flex justify-center items-center | flex-1">
+        <div class="grid grid-cols-4">
+          <AboutMeJobCard
+            class="col-start-1 row-start-1"
+            company="TELUS International"
+            job-role="Software Developer"
+            start-year="Dec 2020"
+          />
+          <AboutMeJobCard
+            class="col-start-2 row-start-2"
+            company="Freelance"
+            job-role="Frontend Developer"
+            start-year="Mar 2020"
+          />
+          <AboutMeJobCard
+            class="col-start-3 row-start-3"
+            company="Master Development"
+            job-role="Frontend Developer"
+            start-year="Oct 2019"
+            end-year="Aug 2020"
+          />
+          <AboutMeJobCard
+            class="col-start-4 row-start-4"
+            company="Viaro Networks"
+            job-role="Quality Assurance"
+            start-year="Jun 2018"
+            end-year="Oct 2019"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+
+@Component
+export default class AboutMeBriefHistory extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.about-me-brief-history {
+  &__title {
+    @extend .gradient-four;
+  }
+}
+
+html.dark-mode {
+  .about-me-brief-history {
+    &__title {
+      @extend .gradient-five;
+    }
+  }
+}
+</style>
---
+++ components/about-me/experience/AboutMeJobCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="inline-block | p-4 | rounded-2xl select-none | bg-gray-100 dark:bg-blue-700 | shadow-md hover:shadow-xl | transition-shadow duration-300">
+    <div class="flex items-center | text-black dark:text-white">
+      <div class="flex flex-col flex-1">
+        <span class="text-lg | font-bold">
+          {{ jobRole }}
+        </span>
+        <span class="text-sm">
+          {{ company }}
+        </span>
+      </div>
+      <div class="flex flex-col | ml-5 | text-xs">
+        <span>
+          {{ startYear }} {{ separatorSymbol }}
+        </span>
+        <span v-if="endYear">
+          {{ endYear }}
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator';
+
+@Component
+export default class AboutMeJobCard extends Vue {
+  @Prop({ type: String, required: true })
+  readonly company!: string;
+
+  @Prop({ type: String, required: true })
+  readonly jobRole!: string;
+
+  @Prop({ type: String, required: true })
+  readonly startYear!: string;
+
+  @Prop({ type: String })
+  readonly endYear!: string;
+
+  get separatorSymbol (): string {
+    return this.endYear ? '/' : '~';
+  }
+}
+</script>
