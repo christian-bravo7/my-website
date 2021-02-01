649cea3ab6fc3b17936713560894bbd2d1171ab0
Christian Bravo <christian151.bravo@gmail.com>
2021-01-31 22:46:22 -0600

    :sparkles: (adding app-select-option component)

--- components/shared/select/AppSelectOption.vue
+++ components/shared/select/AppSelectOption.vue
@@ -1,12 +1,54 @@
 <template>
-  <div>
+  <div
+    ref="itemRef"
+    class="p-2 | hover:text-pink-500 | rounded-2xl | cursor-pointer | transition-all duration-300 | app-select-option"
+    :class="{ active }"
+    @click="selectOption"
+    @keydown.enter="selectOption"
+  >
     <slot />
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'nuxt-property-decorator';
+import { Vue, Component, Inject, Prop } from 'nuxt-property-decorator';
 
 @Component
-export default class AppSelectOption extends Vue {}
+export default class AppSelectOption extends Vue {
+  active: boolean = false;
+
+  @Prop()
+  value!: any;
+
+  @Inject()
+  onSelectOptionClick!: Function;
+
+  selectOption (): void {
+    this.onSelectOptionClick(this);
+  }
+
+  setActive (): void {
+    this.active = true;
+  }
+
+  setInactive (): void {
+    this.active = false;
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+.app-select-option {
+  &:hover {
+    @apply pl-4;
+  }
+
+  &.active {
+    @apply text-pink-500 bg-pink-50;
+  }
+
+  &.active:hover {
+    @apply p-2;
+  }
+}
+</style>
