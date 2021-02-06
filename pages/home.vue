8954d30323aa75a83a921aef9568cc467475de9d
Christian Bravo <christian151.bravo@gmail.com>
2021-02-05 23:17:11 -0600

    :construction: (Adding new icons for branch branding)

--- pages/home.vue
+++ pages/home.vue
@@ -3,15 +3,25 @@
     <Portal to="page-banner">
       <HomeBanner />
     </Portal>
+    <div class="min-h-screen | flex justify-center">
+      <div class="container | bg-gray-100">
+        <NewBranchIcon class="block | w-20 h-20" />
+      </div>
+    </div>
   </section>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator';
 
+import NewBranchIcon from '@/assets/img/new-branch.svg?inline';
+
 import { getMetaTags } from '@/seo';
 
 @Component({
+  components: {
+    NewBranchIcon,
+  },
   head () {
     const homeMetaTags = getMetaTags({
       siteTitle: this.$t('navigation.home-label') as string,
@@ -22,5 +32,5 @@ import { getMetaTags } from '@/seo';
     };
   },
 })
-export default class Index extends Vue {}
+export default class Home extends Vue {}
 </script>
