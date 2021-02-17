3233cd8d40216c6b3dda861cf0453ce4e935b5d5
Christian Bravo <christian151.bravo@gmail.com>
2021-02-16 22:12:20 -0600

    :white_check_mark: (Update file extensions on current tests)

---
+++ test/components/about-me/information/AboutMeInformation.spec.ts
@@ -0,0 +1,29 @@
+import { mount } from '@vue/test-utils';
+
+import AboutMeInformation from '@/components/about-me/information/AboutMeInformation.vue';
+
+describe('AboutMeInformation component', () => {
+  it('should render correctly', () => {
+    const wrapper = mount(AboutMeInformation, {
+      mocks: {
+        $t: jest.fn(msg => msg),
+      },
+    });
+
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('should display title and description', () => {
+    const wrapper = mount(AboutMeInformation, {
+      mocks: {
+        $t: jest.fn(msg => msg),
+      },
+    });
+
+    const title = wrapper.find('h2');
+    const descriptionText = wrapper.find('p');
+
+    expect(title.text()).toBe('me.hi-label');
+    expect(descriptionText.text()).toBe('me.about-me-description');
+  });
+});
