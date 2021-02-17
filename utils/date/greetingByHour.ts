4f397fefb8d63757c159d2ebe4f2c5e299a59dc9
Christian Bravo <christian151.bravo@gmail.com>
2021-02-16 22:11:36 -0600

    :recycle: (Change order for greetingByHours function)

--- utils/date/greetingByHour.ts
+++ utils/date/greetingByHour.ts
@@ -1,19 +1,19 @@
 const greetings: Array<{ greet: string, test: (hours: number) => boolean }> = [
   {
-    greet: 'good-evening',
-    test: (hours: number): boolean => hours >= 18 && hours <= 24,
-  },
-  {
-    greet: 'good-afternoon',
-    test: (hours: number): boolean => hours >= 13 && hours <= 17,
+    greet: 'good-morning',
+    test: (hours: number): boolean => hours >= 0 && hours <= 11,
   },
   {
     greet: 'good-noon',
     test: (hours: number): boolean => hours === 12,
   },
   {
-    greet: 'good-morning',
-    test: (hours: number): boolean => hours >= 0 && hours <= 11,
+    greet: 'good-afternoon',
+    test: (hours: number): boolean => hours >= 13 && hours <= 17,
+  },
+  {
+    greet: 'good-evening',
+    test: (hours: number): boolean => hours >= 18 && hours <= 24,
   },
 ];
 
