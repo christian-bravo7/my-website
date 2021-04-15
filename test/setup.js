aa496a622ba265e6fd2e365f80b2a29c7b3b79a8
Christian Bravo <christian151.bravo@gmail.com>
2021-04-14 22:28:58 -0600

    :white_check_mark: (Update markdown mock function in test files)

--- test/setup.js
+++ test/setup.js
@@ -1,8 +1,30 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { config } from '@vue/test-utils';
 
+import MarkDownIt from 'markdown-it';
+
+const md = new MarkDownIt();
+
+const customRender = (tokens, idx, options, _env, self) =>
+  self.renderToken(tokens, idx, options);
+
+const render = md.renderer.rules.link_open || customRender;
+
+md.renderer.rules.link_open = (tokens, idx, options, env, self) => {
+  const anchorToken = tokens[idx];
+  const hrefUrl = anchorToken.attrGet('href');
+  // Regex matches <schema>:// or schema relative urls //
+  const isUrlAbsolute = !!(hrefUrl && hrefUrl.match(/^(?:[a-zA-Z]+:)?\/\//));
+
+  if (isUrlAbsolute) {
+    anchorToken.attrSet('target', '_BLANK');
+  }
+
+  return render(tokens, idx, options, env, self);
+};
+
 config.mocks = {
-  $md: label => label,
+  $md: string => md.render(string),
   localePath: path => path,
   getRouteBaseName: _ => '/',
 };
