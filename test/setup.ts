3216f0f738f341ef774cf2359c8bf141e483eed6
Christian Bravo <christian151.bravo@gmail.com>
2021-04-15 22:28:48 -0600

    :white_check_mark: (Update jest config and mock nuxt components registry)

--- test/setup.ts
+++ test/setup.ts
@@ -1,40 +1,30 @@
+import path from 'path';
+import glob from 'glob';
+import Vue from 'vue';
 import { config, RouterLinkStub } from '@vue/test-utils';
 
 import { markdownRender } from '@/plugins/markdown';
 
+glob.sync(path.join(__dirname, '../components/**/*.vue')).forEach((file: string) => {
+  const name = file.match(/(\w*)\.vue$/)![1];
+  Vue.component(name, require(file).default);
+});
+
 config.mocks = {
   $md: markdownRender,
+  $colorMode: {
+    value: 'dark',
+  },
   localePath: (path: string): string => path,
   getRouteBaseName: (): string => '/',
 };
 
 config.stubs = {
   NuxtLink: RouterLinkStub,
-  AppNavigationBar: {
-    template: '<div></div>',
-  },
-  AppSidebarMenu: {
-    template: '<div></div>',
-  },
-  AppFallbackLoad: {
-    template: '<div></div>',
-  },
-  PostCardEmpty: {
-    template: '<div></div>',
-  },
-  AppFooter: {
-    template: '<div></div>',
-  },
-  AppContactMe: {
-    template: '<div></div>',
-  },
   Nuxt: {
     template: '<div></div>',
   },
   PortalTarget: {
-    template: '<div></div>',
-  },
-  MovableFigures: {
     template: '<div></div>',
   },
 };
