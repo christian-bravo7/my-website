f2881079b6e7ada243994c7aa5204318677675f4
Christian Bravo <christian151.bravo@gmail.com>
2021-04-17 16:14:19 -0600

    :white_check_mark: (AppFooter component)
    adding unit tests for component

---
+++ test/components/app/footer/AppFooter.spec.ts
@@ -0,0 +1,23 @@
+import { createLocalVue, mount } from '@vue/test-utils';
+
+import AppFooter from '@/components/app/footer/AppFooter.vue';
+
+import setupI18n from '@/test/config/i18n';
+
+const localVue = createLocalVue();
+const i18n = setupI18n(localVue);
+
+const componentConfig = {
+  localVue,
+  i18n,
+};
+
+describe('AppFooter component', () => {
+  it('should render correclty', () => {
+    const wrapper = mount(AppFooter, {
+      ...componentConfig,
+    });
+
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
